c59dd603eae6d2a52653bb3af8ff4b17b1bd2323
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-01 18:42:39 +0600

    Send json as response

--- route/user.route.js
+++ route/user.route.js
@@ -5,6 +5,14 @@ router.get("/login",(req,res)=>{
     res.send('Your are in <h1>Login</h1> Route');
 });
 
+router.get("/json",(req,res)=>{
+    res.status(200).json({
+        "message": "This is Message from json",
+        "statusCode": 200,
+        "Author": "Bulbul Ahammed"
+    })
+});
+
 router.get("/reg",(req,res)=>{
     res.send('You are in <h1>Registration</h1> Route');
 })
