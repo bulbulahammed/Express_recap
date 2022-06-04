f40d832fbb4dea774a2cd174385e0ed4bcf96b2e
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-04 15:26:32 +0600

    query parameter

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const app = require("./app");
-const PORT = 8000;
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/",(req,res)=>{
+    // const id = req.query.id;
+    // const name = req.query.name;
+    // Destructing
+    const {id,name}= req.query;
+    res.send(`<h1>Student ID:${id}, Name is: ${name}</h1>`);
+});
 
 app.listen(PORT,()=>{
-    console.log(`Your express listen at local host ${PORT}`);
+    console.log(`Your Server is running at localhost:${PORT}`);
 })
+
+
+
